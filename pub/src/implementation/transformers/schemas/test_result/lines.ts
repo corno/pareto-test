4a011fbdcac71e702aadf4fc6d9bbb308caf8108
Corno Schraverus <corno@schraverus.com>
2025-12-25 00:54:16 +0100

    ...

--- pub/src/implementation/transformers/schemas/test_result/lines.ts
+++ pub/src/implementation/transformers/schemas/test_result/lines.ts
@@ -3,7 +3,7 @@ import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/sche
 import * as d_in from "../../../../interface/temp/generic"
 
 import * as t_fp from "./fountain_pen"
-import * as t_g_to_lines from "pareto-fountain-pen/dist/implementation/algorithms/transformations/block/lines"
+import * as t_g_to_lines from "pareto-fountain-pen/dist/implementation/transformers/schemas/block/lines"
 
 
 export const Results = ($: d_in.Results): d_out.Lines => {
