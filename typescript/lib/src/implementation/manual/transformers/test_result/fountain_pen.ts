275b1c39da31344522e9c16b3915331a63e1178e
Corno Schraverus <corno@schraverus.com>
2026-06-14 17:12:36 +0200

    pdt: published version 0.13.104

--- typescript/lib/src/implementation/manual/transformers/test_result/fountain_pen.ts
+++ typescript/lib/src/implementation/manual/transformers/test_result/fountain_pen.ts
@@ -1,32 +1,32 @@
-import * as pt from 'pareto-core/dist/assign'
+// import * as pt from 'pareto-core/dist/assign'
 
-import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
-import * as d_in from "../../../../interface/temp/generic"
+// import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+// import * as d_in from "../../../../interface/temp/generic"
 
-import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
+// import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
 
-export const Results = ($: d_in.Results): d_out.Paragraph => Branch($)
+// export const Results = ($: d_in.Results): d_out.Paragraph => Branch($)
 
 
-export const Branch = ($: d_in.Results): d_out.Paragraph => sh.pg.sentences(pt.list.from.dictionary(
-    $,
-).convert(
-    ($, id) => sh.sentence([
-        sh.ph.literal(id),
-        sh.ph.literal(": "),
-        pt.decide.state($, ($) => {
-            switch ($[0]) {
-                case 'test': return pt.ss($, ($) => $.passed
-                    ? sh.ph.literal("✅ PASS")
-                    : sh.ph.literal("❌ FAIL")
-                )
-                case 'group': return pt.ss($, ($) => sh.ph.composed([
-                    sh.ph.indent(
-                        Branch($)
-                    )
-                ]))
-                default: return pt.au($[0])
-            }
-        })
-    ])
-))
+// export const Branch = ($: d_in.Results): d_out.Paragraph => sh.pg.sentences(pt.list.from.dictionary(
+//     $,
+// ).convert(
+//     ($, id) => sh.sentence([
+//         sh.ph.literal(id),
+//         sh.ph.literal(": "),
+//         pt.decide.state($, ($) => {
+//             switch ($[0]) {
+//                 case 'test': return pt.ss($, ($) => $.passed
+//                     ? sh.ph.literal("✅ PASS")
+//                     : sh.ph.literal("❌ FAIL")
+//                 )
+//                 case 'group': return pt.ss($, ($) => sh.ph.composed([
+//                     sh.ph.indent(
+//                         Branch($)
+//                     )
+//                 ]))
+//                 default: return pt.au($[0])
+//             }
+//         })
+//     ])
+// ))
--- typescript/lib/src/interface/temp/generic.ts
+++ typescript/lib/src/interface/temp/generic.ts
@@ -1,40 +1,40 @@
-import * as pi from 'pareto-core/dist/interface'
+// import * as pi from 'pareto-core/dist/interface'
 
-export type Input_And_Expected<In, Expected> = {
-    'input': In
-    'expected': Expected
-}
+// export type Input_And_Expected<In, Expected> = {
+//     'input': In
+//     'expected': Expected
+// }
 
-export type Output_Or_Error<Output, Error> =
-    | ['output', Output]
-    | ['error', Error]
+// export type Output_Or_Error<Output, Error> =
+//     | ['output', Output]
+//     | ['error', Error]
 
-export type Transformer<Input, Expected_Output> = Input_And_Expected<Input, Expected_Output>
+// export type Transformer<Input, Expected_Output> = Input_And_Expected<Input, Expected_Output>
 
-export type Transformer_With_Parameter<Input, Expected_Output, Parameters> = Input_And_Expected<{
-    'input': Input
-    'parameters': Parameters
-}, Expected_Output>
+// export type Transformer_With_Parameter<Input, Expected_Output, Parameters> = Input_And_Expected<{
+//     'input': Input
+//     'parameters': Parameters
+// }, Expected_Output>
 
-export type Refiner_Without_Parameters<Expected_Result, Expected_Error, Input> = Input_And_Expected<Input, Output_Or_Error<Expected_Result, Expected_Error>>
+// export type Refiner_Without_Parameters<Expected_Result, Expected_Error, Input> = Input_And_Expected<Input, Output_Or_Error<Expected_Result, Expected_Error>>
 
-export type Refiner_With_Parameter<Expected_Result, Expected_Error, Input, Parameters> = {
-    'input': {
-        'input': Input
-        'parameters': Parameters
-    }
-    'expected': Output_Or_Error<Expected_Result, Expected_Error>
-}
+// export type Refiner_With_Parameter<Expected_Result, Expected_Error, Input, Parameters> = {
+//     'input': {
+//         'input': Input
+//         'parameters': Parameters
+//     }
+//     'expected': Output_Or_Error<Expected_Result, Expected_Error>
+// }
 
-// Type definitions for test results
+// // Type definitions for test results
 
-export type Results = Branch
+// export type Results = Branch
 
-export type Branch = pi.Dictionary<Result_Entry>
+// export type Branch = pi.Dictionary<Result_Entry>
 
-export type Result_Entry =
-    | ['test', {
-        'passed': boolean
-    }]
-    | ['group', Branch]
+// export type Result_Entry =
+//     | ['test', {
+//         'passed': boolean
+//     }]
+//     | ['group', Branch]
 
