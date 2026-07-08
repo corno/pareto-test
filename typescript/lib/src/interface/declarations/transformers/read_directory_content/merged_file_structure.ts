76c0f30c70149a3cff6880621212378da8795890
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:36:58 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/read_directory_content/merged_file_structure.ts
+++ typescript/lib/src/interface/declarations/transformers/read_directory_content/merged_file_structure.ts
@@ -1,1 +1,23 @@
-import type * as p_ from 'pareto-core/interface/transformer'
+import * as p_ from 'pareto-core/implementation/transformer'
+import type * as p_i from 'pareto-core/interface/transformer'
+import p_implement_me from 'pareto-core-dev/implement_me'
+
+//data types
+import type * as d_in from "pareto-filesystem-unrestricted-api/interface/data/directory_content"
+import type * as d_out from "../../../../interface/data/merged_filesystem_nodes.js"
+
+export namespace interface_ {
+    export type Directory = p_i.Transformer_With_Parameter<
+        d_in.Directory,
+        d_out.Valid_Directory,
+        {
+            'support': d_in.Directory
+
+            /**
+             * the file suffix makes it possible to match a support file that is expected to have a suffix.
+             * for example given the suffix 'foo': /a/b/c.txt.foo is matched to /a/b/c.txt
+             */
+            'support suffix': string
+        }
+    >
+}
